112bc9fffb66a0ece398b96df3a1feb5d2932e08
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-09-17 11:47:02 +0900

    fix: Change a go's package bin env

--- home-config/zsh/.zsh.d/env.zsh
+++ home-config/zsh/.zsh.d/env.zsh
@@ -2,7 +2,7 @@ export PATH="$HOME/.local/bin:$PATH"
 export EDITOR="nvim"
 
 # golang
-export PATH="$(go env GOPATH):$PATH"
+export PATH="$(go env GOBIN):$PATH"
 
 # fzfが隠しファイルも検索する
 export FZF_DEFAULT_COMMAND="find -L"
