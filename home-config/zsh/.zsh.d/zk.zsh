62e263499438a35714fbb80914231994d656fec8
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-09-22 08:39:33 +0900

    refactor: Deduplicate the zk storage directory variable
    What
    - Move the lines that specify the  zk storage directory to the top level.

--- home-config/zsh/.zsh.d/zk.zsh
+++ home-config/zsh/.zsh.d/zk.zsh
@@ -1,18 +1,37 @@
 # ===
 # zk
 # ===
-alias zknt="zk new --title"
 
+# ---
+# Initialize
+# ---
+zk_document_stored_dir="$HOME/dev/zkhub"
+
+if [[ ! -d "$zk_document_stored_dir" ]];then
+	echo "$zk_document_stored_dir is not found."
+	echo "Create $zk_document_stored_dir directory or clone dotfiles into dev directory"
+fi	
+
+alias zkn="zk-new-title"
+# ---
+# Create a new md file into the zkhub directory
+# ---
+function zk-new-title(){
+		if [[ -z "$1" ]];then
+			echo "zkで作成するタイトルを指定してください"
+			return
+		fi
+		local cwd="$(pwd)"
+		cd "$zk_document_stored_dir"
+		zk new --title "$1"
+		cd "$cwd"
+
+}
 # ---
 # Move to the zk stored directory
 # ---
 function zkcd(){
-	local zk_document_stored_dir="$HOME/dev/zkhub"
-	if [[ -d "$zk_document_stored_dir" ]];then
 		cd "$zk_document_stored_dir"
-	else
-		echo "$zk_document_stored_dir is not found."
-	fi
 }
 
 # ---
