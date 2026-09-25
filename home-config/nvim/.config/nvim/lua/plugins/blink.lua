ac48cd4820c2550d89c4d6b07774602e4329e406
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-09-26 08:33:28 +0900

    feat: Split suggestion.lua
    Why
    - It is hard to understand what the file includes from file name.
    
    What
    - Add `friendly-snippets.lua`.
    - Rename `friendly-snippets.lua` to `blink.lua`.
    - Delete `friendly-snippets.lua` config from `blink.lua`.

---
+++ home-config/nvim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,28 @@
+return {
+  "saghen/blink.cmp",
+  lazy = false,
+  opts = {
+    completion = {
+      trigger = {
+        prefetch_on_insert = false,
+      },
+    },
+    keymap = {
+      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      ["<C-Space>"] = { "show" },
+    },
+    sources = {
+      default = { "lsp", "path" },
+      providers = {
+        buffer = { enabled = false },
+      },
+    },
+    fuzzy = {
+      implementation = "lua",
+      sorts = { "exact", "score", "label", "sort_text" },
+      use_frecency = false,
+      use_proximity = false,
+    },
+  },
+}
